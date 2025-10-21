a5b9a25586e8fede9841f141404d3dd3b222ff2f
arutepsu <cgmartchon@gmail.com>
2025-10-21 17:35:49 +0200

    applying template engine

--- app/controllers/UiController.scala
+++ app/controllers/UiController.scala
@@ -2,13 +2,27 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import play.filters.csrf.CSRFAddToken
+import play.api.data._
+import play.api.data.Forms._
+import services.WebTui
 
 @Singleton
-class UiController @Inject()(cc: ControllerComponents, addToken: CSRFAddToken)
-  extends AbstractController(cc) {
+class UiController @Inject()(
+  cc: MessagesControllerComponents,
+  webTui: WebTui
+) extends MessagesAbstractController(cc) {
 
-  def index: Action[AnyContent] = addToken(Action { implicit req =>
-    Ok(views.html.index("Soccer Card Clash Web"))
-  })
+  def index: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
+    webTui.bootOnce()
+    Ok(views.html.index("Soccer Card Clash Web", webTui.snapshot(), ""))
+  }
+
+  def submit: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
+    val cmd = request.body.asFormUrlEncoded
+      .flatMap(_.get("command").flatMap(_.headOption))
+      .getOrElse("")
+    val out = webTui.runAndDrain { webTui.processInputLine(cmd) }
+    Ok(views.html.index("Soccer Card Clash Web", out, ""))
+  }
+
 }
